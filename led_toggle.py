62fc9dd1e331ea94ca1cb80ac932bfe91587351f
hrrhan002 <hrrhan002@gmail.com>
2020-09-26 23:56:51 +0200

    Toggle LED
    Toggle LED ON and OFF every second in an infinite loop

--- led_toggle.py
+++ led_toggle.py
@@ -1,10 +1,17 @@
 import RPi.GPIO as GPIO # Import pi GPIO library
+from time import sleep
 
 def main():
 	GPIO.setmode(GPIO.BOARD) # Pin numbering mode set to board
 	GPIO.setwarnings(False) # Ignore warnings
 
 	GPIO.setup(8, GPIO.OUT, initial=GPIO.LOW) # Set pin 8 as out, initially LOW
+
+	while True:
+		GPIO.output(8, GPIO.HIGH) # LED ON
+		sleep(0.5) # Sleep for 0.5 seconds
+		GPIO.output(8, GPIO.LOW) # LED OFF
+		sleep(0.5) # Sleep for 0.5 seconds
 
 if __name__ == '__main__':
 	main()
