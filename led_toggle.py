8f47b955ce9098b119cb1d35e093cb2cd5da21e6
hrrhan002 <hrrhan002@gmail.com>
2020-09-26 23:27:41 +0200

    Init GPIO
    Set pin numbering mode and warning mode, and initialize pin 8 as output

--- led_toggle.py
+++ led_toggle.py
@@ -1,7 +1,10 @@
 import RPi.GPIO as GPIO # Import pi GPIO library
 
 def main():
-	# this is the main method
+	GPIO.setmode(GPIO.BOARD) # Pin numbering mode set to board
+	GPIO.setwarnings(False) # Ignore warnings
+
+	GPIO.setup(8, GPIO.OUT, initial=GPIO.LOW) # Set pin 8 as out, initially LOW
 
 if __name__ == '__main__':
 	main()
