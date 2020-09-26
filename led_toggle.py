741264cd3f0ce5c9a6d383f109c84dd930bbfbc0
hrrhan002 <hrrhan002@gmail.com>
2020-09-27 00:47:40 +0200

    Add callback for pin 10
    Add an event detection and callback for pin 10, where pushbutton is. No loop atm, doesn't do much

--- led_toggle.py
+++ led_toggle.py
@@ -6,8 +6,14 @@ def main():
 	GPIO.setwarnings(False) # Ignore warnings
 
 	GPIO.setup(8, GPIO.OUT, initial=GPIO.LOW) # Set pin 8 as out, initially LOW
-	GPIO.setup(10, GPIO.IN, pull_up_down=GPIO.PUD_UP) # Set pin 10 as in, with pullup resistor
+	GPIO.setup(10, GPIO.IN, pull_up_down=GPIO.PUD_UP) # Set pin 10 as in, with pulldown resistor
 
+	GPIO.add_event_detect(10, GPIO.FALLING, callback=pushbutton_callback) # Interrupt at button push
+
+	GPIO.cleanup()
+
+def pushbutton_callback(channel):
+	GPIO.output(8, not GPIO.input(8))
 
 if __name__ == '__main__':
 	main()
